f0963dfc46d1a57b0b793f670b60351659a9954a
imon-sheikh <mdimonhossain2017@gmail.com>
2024-10-01 12:02:07 +0600

    added problem 4

---
+++ 18-es6-forEach-filter-find-map-reduce/3-problem.js
@@ -0,0 +1,35 @@
+/**
+ * You have an array of objects :
+ * const instructor = [
+    {name: "Nodi", age: 28, position: "Senior"}
+    {name: "Akhi", age: 26, position: "Junior"}
+    {name: "Shobuj", age: 30, position: "Senior"}
+]
+    Your task is to display the instructor names that has the position senior using filter
+ */
+
+const instructor = [
+    {name: "Nodi", age: 28, position: "Senior"},
+    {name: "Akhi", age: 26, position: "Junior"},
+    {name: "Shobuj", age: 30, position: "Senior"}
+]
+
+const seniors = instructor.filter(single => single.position === "Senior").map(single => single.name)
+console.log(seniors);
+
+
+
+// alternative 
+const instructors = [
+    {name: "Nodi", age: 28, position: "Senior"},
+    {name: "Akhi", age: 26, position: "Junior"},
+    {name: "Shobuj", age: 30, position: "Senior"}
+];
+
+// Using forEach after filtering
+const seniorInstructors = instructors.filter(instructor => instructor.position === "Senior");
+
+seniorInstructors.forEach(instructor => {
+    console.log(instructor.name);
+    console.log(typeof instructor.name);
+});
